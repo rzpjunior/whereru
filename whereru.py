cb2a6ce5e4ff7b7beada3d48ca08e6b45db6d553
junior <junior@edenfarm.id>
2023-09-06 22:21:03 +0700

    initial d

---
+++ whereru.py
@@ -0,0 +1,19 @@
+import socket
+import requests
+import pprint
+import json
+
+hostname = input('Enter domain name: ')
+ip_address = socket.gethostbyname(hostname)
+
+request_url = 'https://geolocation-db.com/jsonp/' + ip_address
+response = requests.get(request_url)
+geolocation = response.content.decode()
+geolocation = geolocation.split("(")[1].strip(")")
+geolocation = json.loads(geolocation)
+
+for k,v in geolocation.items():
+	pprint.pprint(str(k) + ' : ' +str(v))
+
+print(f'Domain : {hostname}')
+print(f'IP Address: {ip_address}')
